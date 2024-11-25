7fb6e1c959119b2639ee6bdf373fc5fabe4cc1aa
Nikesh Sitaula <sitaulanike@gmail.com>
2024-11-25 13:11:30 -0500

    added log4j

---
+++ wb8-exercices/wb8demos/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+public class Main {
+
+    final static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        logMeLikeYouDo("☕");
+    }
+
+
+    private static void logMeLikeYouDo(String input){
+        if(logger.isDebugEnabled()){
+            logger.debug("This is debug : " + input);
+        }
+        if(logger.isInfoEnabled()){
+            logger.info("This is info : " + input);
+        }
+        logger.warn("This is warn : " + input);
+        logger.error("This is error : " + input);
+        logger.fatal("This is fatal : " + input);
+    }
+}
